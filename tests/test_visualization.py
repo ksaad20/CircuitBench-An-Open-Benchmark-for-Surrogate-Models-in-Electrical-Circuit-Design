a6851e43cdb5f029f5aa8205410a12e01be5675b
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-08 08:17:39 +0600

    Update test_visualization.py

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -1,1 +1,16 @@
+"""
+Visualization tests.
+"""
 
+import matplotlib.pyplot as plt
+
+
+def test_plot_creation():
+
+    fig = plt.figure()
+
+    plt.plot([1, 2, 3], [2, 4, 6])
+
+    assert len(fig.axes) == 1
+
+    plt.close(fig)
