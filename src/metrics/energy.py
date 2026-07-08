61ed76727b9cca89ff2ca6bf6f01850e43e42c17
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-08 10:51:57 +0600

    Add energy metrics functions for calculations

--- src/metrics/energy.py
+++ src/metrics/energy.py
@@ -1,1 +1,15 @@
+"""
+Energy metrics.
+"""
 
+
+def joules(power_watts, seconds):
+    return power_watts * seconds
+
+
+def energy_per_prediction(power, runtime, predictions):
+    return joules(power, runtime) / predictions
+
+
+def power_efficiency(predictions, joules_used):
+    return predictions / joules_used
