b99305896100ca5277950fa0ae54a3f2b9b44328
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-08 08:17:03 +0600

    Implement test for repository folder structure
    Add a test for the existence of required folders in the repository structure.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,1 +1,20 @@
+"""
+Utility function tests.
+"""
 
+from pathlib import Path
+
+
+def test_repository_structure():
+
+    folders = [
+        "datasets",
+        "docs",
+        "circuits",
+        "benchmarks",
+        "tests",
+    ]
+
+    for folder in folders:
+
+        assert Path(folder).exists()
