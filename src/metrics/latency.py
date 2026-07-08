c234b8faa5bc8e051bff49164542c88c90b7ee6f
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-08 10:49:51 +0600

    Add latency measurement functions

--- src/metrics/latency.py
+++ src/metrics/latency.py
@@ -1,1 +1,19 @@
+"""
+Latency measurements.
+"""
 
+import time
+
+
+def measure(function, *args, **kwargs):
+    start = time.perf_counter()
+    function(*args, **kwargs)
+    end = time.perf_counter()
+    return end - start
+
+
+def average_latency(function, n=100, *args, **kwargs):
+    values = []
+    for _ in range(n):
+        values.append(measure(function, *args, **kwargs))
+    return sum(values) / len(values)
